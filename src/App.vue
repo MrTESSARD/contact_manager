e09158f40e3e8b21ef87c23b2abd78a946a328ac
MrTESSARD <dimitri@tessard.pro>
2023-12-12 19:02:04 +0100

    Création d'un Single File Component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div id="app">
+<Contact-form/>
+
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ContactForm from './components/ContactForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ContactForm
   }
 }
 </script>
